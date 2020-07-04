d09f2f80a7b93e449abbe52e5142920fac24e6ad
Leonard Shen <uvstudio2199@gmail.com>
2020-07-04 15:21:58 -0400

    added dynamic navbar and forecast

--- public/jquery.js
+++ public/jquery.js
@@ -1,15 +1,26 @@
-// $(() => {
-//   $('.navbar').offset({ top: -50 });
-//   $('.navbar').on('mouseover', (e) => {
-//     if (e) {
-//       $('.navbar').animate({ top: '0px' }, 'fast');
-//     }
-//   });
-//   $('.navbar').on('mouseleave', (e) => {
-//     if (e) {
-//       setTimeout(() => {
-//         $('.navbar').animate({ top: '-50px' }, 'fast');
-//       }, 3000);
-//     }
-//   });
-// });
+$(() => {
+  //navbar animation, show and hide
+  $('.nav-slide').on('click', () => {
+    if ($('.navbar').hasClass('show') && $('.navbar').hasClass('expanded')) {
+      $('.navbar').animate({ top: '-140px' }, 'fast').toggleClass('show');
+    } else if ($('.navbar').hasClass('show')) {
+      $('.navbar').animate({ top: '-70px' }, 'fast').toggleClass('show');
+    } else {
+      $('.navbar').animate({ top: '0px' }, 'fast').toggleClass('show');
+    }
+  });
+  $('.navbar-toggler').on('click', () => {
+    $('.navbar').toggleClass('expanded');
+    if ($('.navbar').hasClass('expanded')) {
+      $('.nav-slide').animate({ top: '160px' }, 'fast');
+    } else {
+      $('.nav-slide').animate({ top: '80px' }, 'fast');
+    }
+  });
+  //current / forecast dynamic text
+  const active = document.getElementsByClassName('active');
+  $('.nav-link').html(active[0].innerHTML);
+  $('.dropdown-menu').on('click', (e) => {
+    $('.nav-link').html(e.target.textContent);
+  });
+});
